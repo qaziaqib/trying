5cd790b0967be568d4a68f075004f6909d8b730e
IBM <IBM@IBM-Testing27>
2018-07-10 11:48:23 +0530

    tryagaintill

---
+++ maveen/src/test/java/maveen/jenkin.java
@@ -0,0 +1,38 @@
+package maveen
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+
+public class jenkin {
+WebDriver driver;
+	
+  @Test
+  public void f() {
+	  
+	  driver.findElement(By.name("userName")).sendKeys("demo");
+	  driver.findElement(By.name("password")).sendKeys("demo");
+	  driver.findElement(By.name("login")).click();
+  }
+  
+  
+  
+  @BeforeTest
+  public void beforeTest() {
+	 driver=new FirefoxDriver();
+	 driver.manage().window().maximize();
+	 driver.get("http://newtours.demoaut.com/index.php");
+	 
+	  
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  
+	  driver.close();
+  }
+
+}
